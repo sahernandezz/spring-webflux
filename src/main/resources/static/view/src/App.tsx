510709a7c9d4cd59d275904405df2907fe187843
alejo <alejo.80.123@hotmail.com>
2023-01-22 17:47:17 -0500

    view

--- src/main/resources/static/view/src/App.tsx
+++ src/main/resources/static/view/src/App.tsx
@@ -2,10 +2,12 @@ import {Fragment, useEffect, useState} from 'react'
 import './App.css'
 import Item from './components/item/Item'
 import ItemPreload from "./components/item/ItemPreload";
+import Particle from "./components/particle/Particle";
 
 function App() {
 
     const [items, setItems] = useState<any[]>([]);
+    const [time, setTime] = useState<string>('');
 
     useEffect(() => {
         const listPreLoad: any[] = [];
@@ -20,13 +22,22 @@ function App() {
                 listI.push(<Item/>);
             }
             setItems(listI);
-        }, 3000);
+        }, 5000);
 
     }, []);
 
-
     return (
         <Fragment>
+            <div className="grid place-items-center bg-gray-800">
+                <Particle
+                    url={"google.png"}
+                    height={500}
+                    scale={0.55}
+                    entropy={15}
+                    maxParticles={10000}
+                />
+                <h1>{time}</h1>
+            </div>
             <div className="grid grid-cols-5 gap-5 m-7">
                 {items}
             </div>
---
+++ src/main/resources/static/view/src/components/particle/Particle.tsx
@@ -0,0 +1,61 @@
+import useWindowSize from "@rooks/use-window-size";
+import * as React from "react";
+
+export interface OptionsParticle {
+    url: string;
+    background?: string | undefined;
+    color?: string | undefined;
+    width?: number | undefined;
+    height?: number | undefined;
+    maxParticles?: number | undefined;
+    scale?: number | undefined;
+    entropy?: number | undefined;
+    className?: string | undefined;
+}
+
+import ParticleImage, {
+    ParticleOptions,
+    Vector,
+    forces,
+    ParticleForce
+} from "react-particle-image";
+
+const Particle = (props: OptionsParticle) => {
+    const particleOptions: ParticleOptions = {
+        filter: ({x, y, image}) => {
+            // Get pixel
+            const pixel = image.get(x, y);
+            // Make a particle for this pixel if blue > 50 (range 0-255)
+            return pixel.b > 50;
+        },
+        color: ({x, y, image}) => props.color != null ? props.color : '#00fdfd',
+        radius: () => Math.random() * 1.5 + 0.5,
+        mass: () => 40,
+        friction: () => 0.15,
+        initialPosition: ({canvasDimensions}) => {
+            return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
+        }
+    };
+
+    const motionForce = (x: number, y: number): ParticleForce => {
+        return forces.disturbance(x, y, 5);
+    };
+
+    const {innerWidth, innerHeight} = useWindowSize();
+
+    return (
+        <ParticleImage
+            src={props.url}
+            width={props.width != null ? props.width : Number(innerWidth) - 9}
+            height={props.height != null ? props.height : Number(innerHeight)}
+            scale={props.scale != null ? Number(innerWidth) - 9 > 1000 ? props.scale : 0.20 : Number(innerWidth) - 9 > 1000 ? 0.35 : 0.20}
+            entropy={props.entropy != null ? props.entropy : 20}
+            maxParticles={props.maxParticles != null ? props.maxParticles : 4000}
+            particleOptions={particleOptions}
+            mouseMoveForce={motionForce}
+            touchMoveForce={motionForce}
+            backgroundColor={props.background != null ? props.background : 'transparent'}
+        />
+    );
+}
+export default Particle;
